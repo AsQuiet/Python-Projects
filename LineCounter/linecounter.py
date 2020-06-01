d8a7986403d3e0884e59a4f981a2fcd601d8ee68
AsQuiet <65712624+AsQuiet@users.noreply.github.com>
2020-06-01 10:36:14 +0200

    Fixed the linecounter

--- LineCounter/linecounter.py
+++ LineCounter/linecounter.py
@@ -3,10 +3,18 @@ import sys
 import os
 path = sys.argv[1]
 
+ignore = []
+for line in open("ignore.txt", "r"):
+	ignore.append(line)
+print(ignore)
+
 print("checking the folder " + path)
 
-
-
+def check(fileName):
+	global ignore
+	for d in ignore:
+		if d.rstrip() in fileName:return False
+	return True
 
 def countLines(file):
 	n = 0
@@ -17,14 +25,15 @@ def countLines(file):
 
 
 def countLinesInFolder(p):
-
+	global ingore
 	files = os.listdir(p)
 	total = 0
 	print("filing :  " + p)
 	print("--------------")
 	for file in files:
 		print(file)
-		if file == ".DS_Store" or file == "p5.js" or file == "brief.js" or file == "brief_compressed.js" or ".wav" in file or ".mp3" in file or ".jpg" in file or ".png" in file : 
+		# if file == ".DS_Store" or file == "p5.js" or file == "brief.js" or file == "brief_compressed.js" or ".wav" in file or ".mp3" in file or ".jpg" in file or ".png" in file : 
+		if not check(file):
 			print("skipping file : " + file + " at : " + str(total))
 			continue
 
